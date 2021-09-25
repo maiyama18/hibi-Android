23df61dca49a38329daf8bef17cb1e19c7cd9c20
maiyama18 <myam0118@gmail.com>
2021-09-25 22:58:56 +0900

    add memos LiveData to view model

--- app/src/main/java/com/muijp/hibi/ui/memolist/MemoListFragment.kt
+++ app/src/main/java/com/muijp/hibi/ui/memolist/MemoListFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.muijp.hibi.database.getDatabase
 import com.muijp.hibi.databinding.MemoListFragmentBinding
+import com.muijp.hibi.repository.MemoRepository
+import timber.log.Timber
 
 class MemoListFragment : Fragment() {
     private lateinit var viewModel: MemoListViewModel
@@ -15,7 +18,10 @@ class MemoListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(MemoListViewModel::class.java)
+        val database = getDatabase(requireActivity().application)
+        val repository = MemoRepository(database)
+        viewModel = ViewModelProvider(this, MemoListViewModelFactory(repository))
+            .get(MemoListViewModel::class.java)
 
         val binding = MemoListFragmentBinding.inflate(inflater)
         binding.viewModel = viewModel
--- app/src/main/java/com/muijp/hibi/ui/memolist/MemoListViewModel.kt
+++ app/src/main/java/com/muijp/hibi/ui/memolist/MemoListViewModel.kt
@@ -1,7 +1,23 @@
 package com.muijp.hibi.ui.memolist
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.muijp.hibi.database.memo.Memo
+import com.muijp.hibi.repository.MemoRepository
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
 
-class MemoListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MemoListViewModel(
+    private val repository: MemoRepository,
+): ViewModel() {
+    val memos: LiveData<List<Memo>> = repository.observeAll()
+}
+
+class MemoListViewModelFactory(private val repository: MemoRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MemoListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MemoListViewModel(repository) as T
+        }
+        throw IllegalArgumentException("unable to construct MemoListViewModel")
+    }
 }
