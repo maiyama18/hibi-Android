13eee38125522b12f7e22488737973c61f62a65d
maiyama18 <myam0118@gmail.com>
2021-10-09 22:09:49 +0900

    add empty view for MemoSearch

--- app/src/main/java/com/muijp/hibi/ui/memosearch/MemoSearchFragment.kt
+++ app/src/main/java/com/muijp/hibi/ui/memosearch/MemoSearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.muijp.hibi.R
 import com.muijp.hibi.databinding.MemoSearchFragmentBinding
 import com.muijp.hibi.ui.recyclerview.memolist.MemoListAdapter
 import com.muijp.hibi.ui.recyclerview.memolist.MemoListListener
@@ -28,8 +29,22 @@ class MemoSearchFragment : Fragment() {
         }
         val adapter = MemoListAdapter(memoListListener)
         binding.memoSearchedListRecyclerView.adapter = adapter
-        viewModel.items.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
+        viewModel.items.observe(viewLifecycleOwner) { memoItems ->
+            if (memoItems.isEmpty()) {
+                binding.memoSearchedListRecyclerView.visibility = View.GONE
+                binding.emptySearchResultTextView.visibility = View.VISIBLE
+
+                val query = viewModel.query.value.orEmpty()
+                if (query.isEmpty()) {
+                    binding.emptySearchResultTextView.text = getString(R.string.input_search_query)
+                } else {
+                    binding.emptySearchResultTextView.text = getString(R.string.no_search_result, query)
+                }
+            } else {
+                binding.memoSearchedListRecyclerView.visibility = View.VISIBLE
+                binding.emptySearchResultTextView.visibility = View.GONE
+            }
+            adapter.submitList(memoItems)
         }
 
         viewModel.query.observe(viewLifecycleOwner) {
