0f99948c5839fb49beafc3b40e1df14c153d8eca
maiyama18 <myam0118@gmail.com>
2021-10-20 23:08:52 +0900

    add skeleton of MemoList

--- app/src/main/java/com/muijp/hibi/ui/memolist/MemoListScreen.kt
+++ app/src/main/java/com/muijp/hibi/ui/memolist/MemoListScreen.kt
@@ -1,12 +1,30 @@
 package com.muijp.hibi.ui.memolist
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MemoListScreen() {
-    Scaffold() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "メモ一覧") },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Search, contentDescription = "メモ検索")
+                    }
+                },
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Filled.Edit, contentDescription = "メモ作成")
+            }
+        }
+    ) {
         Text("MemoList")
     }
 }
