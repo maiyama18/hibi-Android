c35fb7e1882d31d220138fdbafa8e21db9a4ddbf
maiyama18 <myam0118@gmail.com>
2021-10-23 20:09:04 +0900

    fix test of MemoList

--- app/src/test/java/com/muijp/hibi/ui/memolist/MemoListViewModelTest.kt
+++ app/src/test/java/com/muijp/hibi/ui/memolist/MemoListViewModelTest.kt
@@ -1,114 +1,65 @@
 package com.muijp.hibi.ui.memolist
 
-//class MemoListViewModelTest {
-//    @get:Rule
-//    val testRule = InstantTaskExecutorRule()
-//
-//    @ExperimentalCoroutinesApi
-//    @get:Rule
-//    val mainCoroutineRule = MainCoroutineRule()
-//
-//    @MockK
-//    lateinit var memoRepository: MemoRepository
-//
-//    @Before
-//    fun setUp() = MockKAnnotations.init(this)
-//
-//    @Test
-//    fun init() {
-//        // given
-//        every {
-//            memoRepository.liveDataByLimit(10)
-//        } returns MutableLiveData(
-//            (1..10).map { i-> Memo("memoId$i", "this is memo $i", ZonedDateTime.now(), ZonedDateTime.now()) }
-//        )
-//
-//        // when
-//        val viewModel = MemoListViewModel(memoRepository)
-//
-//        // then
-//        viewModel.items.test()
-//            .assertHasValue()
-//            .assertValue { it.size == 11 }
-//            .assertValue { it.last().id == "memoId10" }
-//
-//        confirmVerified()
-//    }
-//
-//    @Test
-//    fun onScrolledToBottom_allMemosAlreadyShown() {
-//        // given
-//        every {
-//            memoRepository.liveDataByLimit(10)
-//        } returns MutableLiveData(
-//            (1..10).map { i-> Memo("memoId$i", "this is memo $i", ZonedDateTime.now(), ZonedDateTime.now()) }
-//        )
-//
-//        coEvery {
-//            memoRepository.count()
-//        } returns 10
-//
-//        val viewModel = MemoListViewModel(memoRepository)
-//
-//        viewModel.items.test()
-//            .awaitValue()
-//            .assertHasValue()
-//            .assertValue { it.size == 11 }
-//
-//        // when
-//        viewModel.onScrolledToBottom()
-//
-//        // then
-//        viewModel.items.test()
-//            .assertHasValue()
-//            .assertValue { it.size == 11 }
-//
-//        verify(exactly = 1) { memoRepository.liveDataByLimit(10) }
-//        verify(exactly = 0) { memoRepository.liveDataByLimit(20) }
-//        coVerify(exactly = 1) { memoRepository.count() }
-//        confirmVerified()
-//    }
-//
-//    @Test
-//    fun onScrolledToBottom_readMoreMemos() {
-//        // given
-//        every {
-//            memoRepository.liveDataByLimit(10)
-//        } returns MutableLiveData(
-//            (1..10).map { i-> Memo("memoId$i", "this is memo $i", ZonedDateTime.now(), ZonedDateTime.now()) }
-//        )
-//
-//        coEvery {
-//            memoRepository.count()
-//        } returns 100
-//
-//        every {
-//            memoRepository.liveDataByLimit(20)
-//        } returns MutableLiveData(
-//            (1..20).map { i-> Memo("memoId$i", "this is memo $i", ZonedDateTime.now(), ZonedDateTime.now()) }
-//        )
-//
-//        val viewModel = MemoListViewModel(memoRepository)
-//
-//        viewModel.items.test()
-//            .awaitValue()
-//            .assertHasValue()
-//            .assertValue { it.size == 11 }
-//
-//        // when
-//        viewModel.onScrolledToBottom()
-//
-//        // then
-//        viewModel.items.test()
-//            .awaitNextValue(1, TimeUnit.SECONDS)
-//            .assertHasValue()
-//            .assertValue { it.size == 21 }
-//
-//        verify(exactly = 1) {
-//            memoRepository.liveDataByLimit(10)
-//            memoRepository.liveDataByLimit(20)
-//        }
-//        coVerify(exactly = 1) { memoRepository.count() }
-//        confirmVerified()
-//    }
-//}
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.google.common.truth.Truth.assertThat
+import com.jraska.livedata.test
+import com.muijp.hibi.database.memo.Memo
+import com.muijp.hibi.repository.MemoRepository
+import com.muijp.hibi.ui.utils.MainCoroutineRule
+import io.mockk.MockKAnnotations
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.time.ZonedDateTime
+
+class MemoListViewModelTest {
+    @get:Rule
+    val testRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    @MockK
+    lateinit var memoRepository: MemoRepository
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this)
+
+    @Test
+    fun init() {
+        val today = ZonedDateTime.now()
+        val yesterday = ZonedDateTime.now().minusDays(1)
+        // given
+        every {
+            memoRepository.liveDataByLimit(10)
+        } returns MutableLiveData(
+            (1..10).map { i ->
+                Memo(
+                    "memoId$i",
+                    "this is memo $i",
+                    if (i >= 8) yesterday else today,
+                    if (i >= 8) yesterday else today
+                )
+            }
+        )
+
+        // when
+        val viewModel = MemoListViewModel(memoRepository)
+
+        // then
+        val memos = viewModel.memos.test()
+            .value()
+
+        assertThat(memos.keys.size).isEqualTo(2)
+        assertThat(memos[today.toLocalDate()]?.size).isEqualTo(7)
+        assertThat(memos[yesterday.toLocalDate()]?.size).isEqualTo(3)
+
+        confirmVerified()
+    }
+}
