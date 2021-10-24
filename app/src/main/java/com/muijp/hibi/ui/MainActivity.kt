eee1677cea897f99c74565d0a5582a517678e54f
maiyama18 <myam0118@gmail.com>
2021-10-24 21:27:05 +0900

    fix status bar color

--- app/src/main/java/com/muijp/hibi/ui/MainActivity.kt
+++ app/src/main/java/com/muijp/hibi/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.muijp.hibi.ui.theme.statusBarColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -13,6 +16,12 @@ class MainActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
+
+            SideEffect {
+                systemUiController.setStatusBarColor(color = statusBarColor)
+            }
+
             HibiApp()
         }
     }
